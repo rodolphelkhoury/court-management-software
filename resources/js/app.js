814dacab41f6e8303361160890aa59f616fa0d19
Rodolph Khoury <rodolph@tomorrow.services>
2025-12-21 22:24:18 +0200

    2.2 and 2.3

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,8 @@ import 'leaflet/dist/leaflet.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
+// import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
